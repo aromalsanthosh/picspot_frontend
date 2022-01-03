8936b0ac5142975334b66d75e01163027a9a5cd6
Aromal S <aromalsanthosh@protonmail.com>
2022-01-03 21:56:51 +0530

    Mobile NavBar ✅

--- src/container/Home.jsx
+++ src/container/Home.jsx
@@ -13,6 +13,8 @@ import logo from '../assets/logo-color.png'
 const Home = () => {
     const [ToggleSidebar, setToggleSidebar] = useState(false)
     const [user, setUser] = useState(null)
+    const scrollRef = useRef(null)
+
 
     const userInfo = localStorage.getItem('user') != 'undefined' ? JSON.parse(localStorage.getItem('user')) : localStorage.clear();
     
@@ -25,19 +27,30 @@ const Home = () => {
         })
 
     }, [])
+
+    useEffect(() => {
+      return () => {
+        scrollRef.current.scrollTo(0, 0)
+      };
+    }, [])
+
     return (
         <div className="flex bg-gray-50 md:flex-row flex-col h-screen transition-height duration-75 ease-out">
             <div className="hidden md:flex h-screen flex-initial">
                 <SideBar user={user && user} />
             </div>
             <div className="flex md:hidden flex-row">
-                <HiMenu  fontSize={40} className='cursor-pointer' onClick={() => {setToggleSidebar(true)} } />
-                <Link to='/' >
-                    <img src={logo} className="w-28 mx-auto" alt="logo" />
-                </Link>
-                <Link to={`user-profile/${user?._id}`}>
-                    <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
-                </Link>
+
+                <div className='p-2 w-full flex flex-row justify-between items-center shadow-md'>
+                    <HiMenu  fontSize={40} className='cursor-pointer' onClick={() => {setToggleSidebar(true)} } />
+                    <Link to='/' >
+                        <img src={logo} className="w-28 mx-auto" alt="logo" />
+                    </Link>
+                    <Link to={`user-profile/${user?._id}`}>
+                        <img src={user?.image} alt="user-pic" className="w-9 h-9 rounded-full " />
+                    </Link>
+                </div>
+                
                 {ToggleSidebar && (
                     <div className="fixed w-3/5 bg-white h-screen overflow-y-auto shadow-md z-10 animate-slide-in">
                     <div className="absolute w-full flex justify-end items-center p-2">
@@ -46,9 +59,15 @@ const Home = () => {
                     <SideBar closeToggle={setToggleSidebar} user={user && user} />
                     </div>
                 )}
-                
-
             </div>
+
+            <div className="pb-2 flex-1 h-screen overflow-y-scroll" ref={scrollRef}>
+                <Routes>
+                    <Route path="/user-profile/:userId" element={<UserProfile  />} />
+                    <Route path="/*" element={<Pins user={user && user}  />} />
+                </Routes>
+            </div>
+
         </div>
     )
 }
