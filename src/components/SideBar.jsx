0d7b6e4a8c7f509ae3f8cf113bcc720ac5b3d689
Aromal S <aromalsanthosh@protonmail.com>
2022-01-14 21:49:04 +0530

    Sidebar UI Complete ✅

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -8,25 +8,32 @@ const isNotActiveStyle = 'flex items-center px-5 gap-3 text-gray-500 hover:text-
 const isActiveStyle = 'flex items-center px-5 gap-3 font-extrabold border-r-2 border-black  transition-all duration-200 ease-in-out capitalize'
 
 
-const SideBar = (user, closeToggle) => {
+const SideBar = ({user, closeToggle}) => {
     const categories = [  
         {
             name: 'Animals',
         },
         {
-            name: 'Cars',
+            name: 'Wallpapers',
         },
         {
-            name: 'Fashion',
+            name: 'Photography',
+        },
+        {
+            name: 'Gaming',
+        },
+        {
+            name: 'Coding',
+        },
+        {
+            name: 'Other',
         },
 
     ]
 
     const handleCloseSidebar = () => {
-        if(closeToggle) {
-            closeToggle(false);
-        }
-    }
+        if (closeToggle) closeToggle(false);
+      };
 
     return (
         <div className='flex flex-col justify-between bg-white h-full overflow-y-scroll min-w-210 hide-scrollbar'>
@@ -42,17 +49,39 @@ const SideBar = (user, closeToggle) => {
                     <NavLink
                         to="/"
                         className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
-                        
+                        onClick={handleCloseSidebar}
                     >
                         <RiHomeFill />
                         Home
                     </NavLink>
                     <h3 className="mt-2 px-5 text-base 2xl:text-xl">Discover categories</h3>
-                    
+                    {
+                        categories.slice(0, categories.length - 1).map(category => (
+                            <NavLink
+                                to={`/category/${category.name}`}
+                                className={({ isActive }) => (isActive ? isActiveStyle : isNotActiveStyle)}
+                                onClick={handleCloseSidebar}
+                                key={category.name}
+                            >
+                                {category.name}
+                            </NavLink>
+                        ))
+                    }
 
 
                 </div>
             </div>
+            {user && (
+                <Link
+                to={`user-profile/${user._id}`}
+                className="flex my-5 mb-3 gap-2 p-2 items-center bg-white rounded-lg shadow-lg mx-3"
+                onClick={handleCloseSidebar}
+                >
+                <img src={user.image} className="w-10 h-10 rounded-full" alt="user-profile" />
+                <p>{user.username}</p>
+                <IoIosArrowForward />
+                </Link>
+            )}
         </div>
     )
 }
