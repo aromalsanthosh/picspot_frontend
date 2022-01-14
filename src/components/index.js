63c583d263f4556c62a8ef23c921313dfae662a1
Aromal S <aromalsanthosh@protonmail.com>
2022-01-14 22:16:20 +0530

    exporting new components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,8 @@
 export {default as SideBar } from './SideBar'
 export {default as UserProfile } from './UserProfile'
 export {default as Login } from './Login'
+export { default as Navbar } from './Navbar';
+export { default as Feed } from './Feed';
+export { default as PinDetail } from './PinDetail';
+export { default as CreatePin } from './CreatePin';
+export { default as Search } from './Search';
