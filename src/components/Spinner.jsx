31dac9a0a5f3e4e18853e6ce518a7acbd92f3a02
Aromal S <aromalsanthosh@protonmail.com>
2022-01-15 10:16:04 +0530

    Create loading spinner ➰

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Loader from 'react-loader-spinner';
+
+function Spinner({ message }) {
+    return (
+        <div className="flex flex-col justify-center items-center w-full h-full">
+            <Loader
+            type="Circles"
+            color="#5cb6f9"
+            height={50}
+            width={200}
+            className="m-5"
+            />
+        <p className="text-lg text-center px-2">{message}</p>
+        </div>
+    );
+}
+
+export default Spinner
