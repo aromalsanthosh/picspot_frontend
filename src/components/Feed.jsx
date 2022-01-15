48e9f8138834b6fac8c376cff6f4dfd8fa9f1f9d
Aromal S <aromalsanthosh@protonmail.com>
2022-01-15 10:17:52 +0530

    Create Feed Component ⌛

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+
+import { client } from '../client';
+import { feedQuery, searchQuery } from '../utils/data';
+
+import MasonryLayout from './MasonryLayout';
+import Spinner from './Spinner';
 
 const Feed = () => {
+
+    const [loading, setLoading] = useState(false);
+    const { categoryId } = useParams();
+    const [pins, setPins] = useState(null);
+
+
+    useEffect(() => {
+        if (categoryId) {
+            setLoading(true);
+            const query = searchQuery(categoryId);
+            client.fetch(query).then((data) => {
+                setPins(data);
+                setLoading(false);
+            });
+        } 
+        else {
+            setLoading(true);
+    
+            client.fetch(feedQuery).then((data) => {
+            setPins(data);
+            setLoading(false);
+        });
+        }
+    }, [categoryId]);
+
+    if (loading) {
+        return (
+          <Spinner message={`We are adding new ideas to your feed!`} />
+        );
+    }
+
+
     return (
-        <div>Feed
+        <div>
+            {pins && <MasonryLayout pins={pins} />}
         </div>
     )
 }
