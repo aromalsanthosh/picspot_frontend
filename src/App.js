faab66c7a497588639dab6262193f8ad7aee0f61
Aromal S <aromalsanthosh@protonmail.com>
2022-01-17 12:34:51 +0530

    Fix hosting login issue

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Routes, Route, useNavigate } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './container/Home'
 
+import { fetchUser } from './utils/fetchUser'
+
 function App() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const user = fetchUser();
+    
+        if (!user) navigate('/login');
+      }, [])
+
     return (
         <Routes>
             <Route path="/login" element={<Login />} />
